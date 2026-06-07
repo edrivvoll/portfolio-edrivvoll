ae7ae6894efa4bb498ec6e6350f7fd4d861326df
edrivvoll <espendr@outlook.com>
2026-06-07 21:22:22 +0200

    removed old files

--- js/ham-menu.js
+++
@@ -1,8 +0,0 @@
-const hamMenu = document.querySelector(".ham-menu");
-
-const offScreenMenu = document.querySelector(".menu");
-
-hamMenu.addEventListener("click", () => {
-    hamMenu.classList.toggle("active");
-    offScreenMenu.classList.toggle("active");
-})
